7b1fc8bf63f201f6f73857f5ada8438745888ada
7Gabriel72 <fxgabrielmarques@gmail.com>
2025-08-13 14:58:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function generatePassword() {
+  const length = document.getElementById("length").value;
+  const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  document.getElementById("password").value = password;
+}
+
+function updateLength() {
+  const length = document.getElementById("length").value;
+  document.getElementById("length-value").textContent = length;
+}
